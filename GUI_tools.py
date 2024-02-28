9ce23ae30b092731b782baceb613f22323daa0c4
LasevIX_@Laptop-lOhkM <l73yx.off@gmail.com>
2024-02-28 19:09:38 +0100

    DONE: Vector class

--- GUI_tools.py
+++ GUI_tools.py
@@ -2,8 +2,62 @@ from typing import Any, Literal
 from rendering_pipeline import Sprite
 from input_pipeline import KeyboardInputAgent
 
+class PositionVector:
+    def __init__(
+        self,
+        x: int | None = None,
+        y: int | None = None,
+        xy: tuple[int, int] | None = None,
+    ) -> None:
+        """Creates a position vector from a tuple of integers or 2 integer parameters."""
+        if xy is not None:
+            self.x, self.y = xy
+        elif x is not None and y is not None:
+            self.x, self.y = x, y
 
-# TODO: Vector type instead of tuples
+    def copy(self):
+        return PositionVector(self.x, self.y)
+
+    def __len__(self) -> float:
+        """Returns vector length"""
+        return (self.x**2 + self.y**2) ** 0.5
+
+    def __add__(self, value: "PositionVector |tuple[int,int]") -> "PositionVector":
+        "Returns self+value"
+        result_vector: PositionVector = self.copy()
+        if isinstance(value, tuple):
+            result_vector.x += value[0]
+            result_vector.y += value[1]
+        elif type(value) == type(self):
+            result_vector.x += value.x
+            result_vector.y += value.y
+        else:
+            raise ValueError
+        return result_vector
+
+    def __rmul__(
+        self, value: "float |PositionVector |tuple[int,int]"
+    ) -> "PositionVector|float":
+        "Returns value*self ; for vector multiplication, works as a dot product."
+        result_vector: PositionVector = self.copy()
+        if isinstance(value, (float, int)):
+            result_vector.x *= value
+            result_vector.y *= value
+            return result_vector  # scalar product
+        else:
+            if isinstance(value, tuple):
+                return self.x * value[0] + self.y * value[1]  # dot product
+            elif type(value) == type(self):
+                return self.x * value.x + self.y * value.y  # dot product
+            else:
+                raise ValueError
+    def __radd__(self, value: "PositionVector |tuple[int,int]") -> "PositionVector":
+        return self.__add__(value)
+    def __mul__(
+        self, value: "float |PositionVector |tuple[int,int]"
+    ) -> "PositionVector|float":
+        return self.__rmul__(value)
+
 class GUIElement:
     def __init__(self, parent, name: str, sprite: Sprite, pos: tuple[int, int]) -> None:
         self.name: str = name
